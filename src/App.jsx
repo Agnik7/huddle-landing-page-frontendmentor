ab9461ae04af6f1ff8ebae41ccbed8b1d85bbdb6
Agnik Bakshi <bakshi.agnik@gmail.com>
2024-01-04 15:37:38 +0530

    Register Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Mid from './components/Mid';
+import Register from './components/Register';
 function App() {
-
   return (
     <main className='min-h-screen w-full flex flex-wrap justify-center'>
       <Header />
       <Hero />
       <Mid/>
+      <Register />
     </main>
   )
 }
