55900fb6de5c076c0ed6e06a5a8899c673ead9b0
Agnik Bakshi <bakshi.agnik@gmail.com>
2024-01-04 15:30:17 +0530

    Mid done

---
+++ src/components/InfoCardEven.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function InfoCardEven({title,desc, imgSrc}) {
+  return (
+    <div className='w-full max-w-[90rem] flex flex-row-reverse justify-center pl-[8rem] pb-[2.5rem]'>
+      <div className='w-1/2 flex flex-col justify-center mt-[4rem]'>
+          <h2 className='font-header font-bold text-[2.5rem]'>{title}</h2>
+          <p className='max-w-[32.5rem]'>{desc}</p>
+      </div>
+      <div className='w-1/2 mt-[4.3rem] mx-[1.5rem]'>
+        <img src={imgSrc} alt="Info Img" className='w-4/5' />
+      </div>
+    </div>
+  )
+}
--- src/components/Mid.jsx
+++ src/components/Mid.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
-
+import React from 'react';
+import InfoCardOdd from './InfoCardOdd';
+import InfoCardEven from './InfoCardEven';
+import topDesktop from '../assets/images/bg-section-top-desktop-1.svg';
+import topMobile from '../assets/images/bg-section-top-mobile-1.svg';
+import bottomDesktop from '../assets/images/bg-section-bottom-desktop-1.svg';
+import bottomMobile from '../assets/images/bg-section-top-mobile-1.svg';
+import infoImg1 from '../assets/images/illustration-grow-together.svg';
+import infoImg2 from '../assets/images/illustration-flowing-conversation.svg';
+import infoImg3 from '../assets/images/illustration-your-users.svg';
 export default function Mid() {
   return (
-    <div>Mid</div>
+    <section className='w-full'>
+        <img src={topDesktop} alt="Background Pattern" className='w-full hidden sm:block' />
+        <img src={topMobile} alt="Background Pattern" className='w-full block sm:hidden'/>
+        <div className='w-full bg-neutral-pale-blue flex justify-center flex-wrap'>
+            <InfoCardOdd title="Grow Together" desc="Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form." imgSrc={infoImg1}/>
+        </div>
+        <img src={bottomDesktop} alt="Background Pattern" className='w-full hidden sm:block' />
+        <img src={bottomMobile} alt="Background Pattern" className='w-full block sm:hidden'/>
+        <InfoCardEven title="Flowing Conversations" desc="You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow." imgSrc={infoImg2} />
+        <img src={topDesktop} alt="Background Pattern" className='w-full hidden sm:block' />
+        <img src={topMobile} alt="Background Pattern" className='w-full block sm:hidden'/>
+        <div className='w-full bg-neutral-pale-blue flex justify-center flex-wrap'>
+            <InfoCardOdd title="Your Users" desc="It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately." imgSrc={infoImg3}/>
+        </div>
+        <img src={bottomDesktop} alt="Background Pattern" className='w-full hidden sm:block' />
+        <img src={bottomMobile} alt="Background Pattern" className='w-full block sm:hidden'/>
+    </section>
   )
 }
